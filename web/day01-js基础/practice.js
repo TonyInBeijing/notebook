18863af889a62f3c7e3e5eadd5b6510a6abd7b45
TonyInBeijing <1300565735@qq.com>
2021-03-06 22:47:13 +0800

    day01练习代码

---
+++ web/day01-js基础/practice.js
@@ -0,0 +1,64 @@
+/**
+ * @description day1练习（部分伪代码）
+ */
+
+// 代码执行阶段的主要的工作是：
+// 1.分配变量、函数的引用
+// 2.执行代码
+
+// 例子
+function demo(num) {
+    var name = "xxx";
+    var getData = function getData() { };
+    function c() { }
+}
+demo(100);
+
+// 创建过程大致是
+executionContext = {
+    scopeChain: { ...},
+    variableObject: {
+        arguments: {
+            // 创建了参数对象
+            0: 100,
+            length: 1,
+        },
+        num: 100, // 创建行参名称，赋值/创建引用拷贝
+        c: 'pointer to function c()', // 有内部函数声明的话，创建引用指向函数体
+        name: undefined, //有内部声明变量a，初始化为undefined
+        getData: undefined, //有内部声明变量b，初始化为undefined，
+
+    },
+    this: { ...},
+};
+
+// 代码执行阶段 -- 在这个阶段主要是赋值并执行代码
+executionContext = {
+    scopeChain: { ...},
+    variableObject: {
+        arguments: {
+            0: 100,
+            length: 1,
+        },
+        num: 100,
+        c: 'pointer to function c()',
+        name: 'xxx', // 分配变量，赋值
+        getData: 'pointer to function getData()',// 分配函数的引用，赋值
+        this: { ...}
+    }
+};
+
+// 作用域
+function outer() {
+    var person = 'yuanxin';
+    function inner() {
+        console.log(person);
+    }
+    return inner();
+}
+var inner = outer();
+function execution() {
+    var person = 'xiaowa';
+    inner();
+}
+execution();// 'yuanxin'
