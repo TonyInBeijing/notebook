bbee0f345c690fb2bd23242d73c6deb93ad741fc
TonyInBeijing <1300565735@qq.com>
2021-03-18 22:32:38 +0800

    es6

--- web/day03-es6深入理解/practice.js
+++ web/day03-es6深入理解/practice.js
@@ -2,3 +2,12 @@
  * @description es6 & esNext
  */
 
+var funcs = [];
+for (var i = 0; i < 10; i++) {
+    funcs.push(function () {
+        console.log(i);
+    });
+}
+funcs.forEach(function (func) {
+    func(); //输出10次10
+});
