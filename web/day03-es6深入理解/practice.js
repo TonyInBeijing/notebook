bf76994cf9ae9a85a88058c6cd458d6e8cb9fec9
TonyInBeijing <1300565735@qq.com>
2021-03-22 22:56:53 +0800

    块级作用域

--- web/day03-es6深入理解/practice.js
+++ web/day03-es6深入理解/practice.js
@@ -8,5 +8,12 @@ for (var i = 0; i < 10; i++) {
     });
 }
 funcs.forEach(function (func) {
-    func(); //输出10次10 var变量提成，执行时取外层作用域
+    func(); //输出10次10 var变量提升，执行时取外层作用域
+});
+
+for (var i = 0; i < 10; i++) {
+    funcs.push((function (i) { console.log(i) })(i));
+}
+funcs.forEach(function (func) {
+    func(); //输出0-9 闭包缓存
 });
