7355fdb4eb67951c8bd902ea111b88ea35f81308
TonyInBeijing <1300565735@qq.com>
2021-04-06 23:10:50 +0800

    扩展运算符

--- web/day03-es6深入理解/practice.js
+++ web/day03-es6深入理解/practice.js
@@ -59,7 +59,6 @@ function requestParamsProcess(params) {
     processedParams = {...params}
     return processedParams;
 }
-
 function request() {
     const processedParams = requestParamsProcess(params);
     // 发送请求
