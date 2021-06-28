f34c490dab24653e72abe2c6c075fab0bee259d3
TonyInBeijing <1300565735@qq.com>
2021-06-28 22:26:21 +0800

    Promise基本用法

--- web/day03-es6深入理解/practice.js
+++ web/day03-es6深入理解/practice.js
@@ -91,15 +91,15 @@ class PersonClass {
     sayName() {
         console.log(this.name);
     }
-    static staticFunc(){
+    static staticFunc() {
         console.log("111");
     }
 }
 // es5
 function PersonClass() {
-    
+
 }
-function constructor(name){
+function constructor(name) {
     this.name = name;
 }
 PersonClass.prototype = {
@@ -108,6 +108,72 @@ PersonClass.prototype = {
         console.log(this.name);
     }
 }
-PersonClass.staticFunc = function(){
+PersonClass.staticFunc = function () {
     console.log("222");
 }
+
+// promise
+
+function start() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => { resolve(111) }, 1000);
+    });
+}
+start().then(res => {
+    console.log("res-1::", res);
+    return 222;
+}).then(res2 => {
+    console.log("res-2::", res2);
+    return new Promise((resolve, reject) => {
+        setTimeout(() => { resolve(444) }, 1000);
+    });
+}).then(res3 => {
+    console.log("res-3::", res3); // 444 当上一个then返回一个新的Promise对象，当前then的res是新的Promise的返回值
+    var newPromise = new Promise((resolve, reject) => {
+        setTimeout(resolve(666));
+    });
+    return newPromise.then(res => {
+        return 777;
+    });
+}).then(res4 => {
+    console.log("res-4::", res4); // 777 同理
+    throw new Error("Error!");
+}).then(res5 => {
+    console.log("res-5::", res5);
+}).catch(e => {
+    console.log("Error-1::", e); // 不执行res5的then，直接跳到catch
+    return 666;
+}).then(res6 => {
+    console.log("res-6::", res6); // catch兜住一次错误，后面的then继续执行
+});
+
+
+// promise.all
+
+let p1 = new Promise(resolve => {
+    resolve(1);
+});
+let p2 = new Promise(resolve => {
+    resolve(2);
+});
+let p3 = new Promise(resolve => {
+    resolve(3);
+});
+
+let p4 = Promise.all([p1, p2, p3]); // p4也是一个Promise对象
+p4.then(([p1res, p2res, p3res]) => {
+    console.log(p1res, p2res, p3res); // 1,2,3 p1,p2,p3都决议以后执行
+});
+
+
+// promise.race
+let firstPromise = new Promise(resolve => {
+    setTimeout(resolve(1), 1000);
+});
+let lastPromise = new Promise(resolve => {
+    setTimeout(resolve(2), 2000);
+});
+let resPromise = Promise.race([firstPromise, lastPromise]);
+resPromise.then(firstRes => {
+    console.log(firstRes); // 1 有一个Promise决议后就执行
+});
