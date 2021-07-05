980808c1da4495cd132b63f7f826b2ce001b792d
TonyInBeijing <1300565735@qq.com>
2021-07-05 21:21:32 +0800

    mini-promise

--- web/day04-promise&模块化/mini-promise.js
+++ web/day04-promise&模块化/mini-promise.js
@@ -1,14 +1,27 @@
 /**
  * @description 手写一个promise
  */
-function MiniPromise() {
-
-};
-// then
-MiniPromise.prototype.then = function () { };
-// catch
-MiniPromise.prototype.catch = function () { }
-// all
-MiniPromise.prototype.all = function () { }
-// race
-MiniPromise.prototype.race = function () { }
+(function (global) {
+    function Promise(processor) {
+        this._status = "PENDING";
+        processor(
+            res => {
+                this._resolve(res);
+            },
+            () => {
+                this._status = "REJECTED";
+            }
+        );
+    }
+    Promise.prototype = {
+        then: function (onFulFilled) {
+            this.onFulFilled = onFulFilled;
+        },
+        _resolve: function _resolve(res) {
+            this._status = "FULFILLLED";
+            this.currentValue = res;
+            this.onFulFilled(this.currentValue);
+        }
+    }
+    global.Promise = Promise;
+})(window);
