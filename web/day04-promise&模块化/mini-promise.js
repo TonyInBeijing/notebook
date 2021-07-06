ee40f35c72dbae6b66afd9159fe42d3c7ca3e5bf
TonyInBeijing <1300565735@qq.com>
2021-07-06 22:10:51 +0800

    mini-promise

--- web/day04-promise&模块化/mini-promise.js
+++ web/day04-promise&模块化/mini-promise.js
@@ -14,13 +14,33 @@
         );
     }
     Promise.prototype = {
+        constructor: Promise,
+        _taskCallback: function (value, processor, next) {
+            var preResult = processor(value);
+            if (preResult instanceof Promise) {
+                preResult.then(res => {
+                    next._resolve(res);
+                });
+                return;
+            }
+            return next._resolve(preResult);
+        },
         then: function (onFulFilled) {
             this.onFulFilled = onFulFilled;
+            this.next = new Promise((resolve, reject) => { });
+            if (this._status === "FULFILLED") {
+                this._taskCallback(this.currentValue, this.onFulFilled, this.next);
+            }
+            return this.next;
         },
         _resolve: function _resolve(res) {
-            this._status = "FULFILLLED";
+            this._status = "FULFILLED";
             this.currentValue = res;
-            this.onFulFilled(this.currentValue);
+            if (this.onFulFilled) {
+                this._taskCallback(res, this.onFulFilled, this.next);
+                // this.onFulFilled(this.currentValue);
+            }
+
         }
     }
     global.Promise = Promise;
