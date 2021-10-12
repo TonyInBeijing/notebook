879035c6cf41b644b02078390e035c7ad6349aa3
TonyInBeijing <yuehaowei1126@gmail.com>
2021-10-13 00:13:50 +0800

    廖雪峰Python教程笔记

---
+++ 廖雪峰Python教程/Python基础/ basement.py
@@ -0,0 +1,31 @@
+# 数据类型和变量
+
+# 转义字符 \
+print('I\'m studing python');
+print('I\'m \n studing python');
+
+# 多行打印
+print('''111
+222
+333''')
+
+# 布尔值 and or not
+print(True and True)
+print(False or True)
+print(not True)
+
+# 空值 None
+print(None);
+
+# 定义变量
+a = 1
+print(a)
+
+# 除法
+# / 精确除法
+b = 2;
+print(b/1) # 2.0
+# // 只取整数部分
+print(b//3) # 0
+# & 取余
+print(b%3)  # 2
---
+++ 廖雪峰Python教程/README.md
@@ -0,0 +1,2 @@
+# 廖雪峰Python教程学习笔记
+## 为Tensorflow快速学习一下python语法
