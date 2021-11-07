476d9a298700d0cbe5d6d0d78bf5e4657b81fbdf
TonyInBeijing <yuehaowei1126@gmail.com>
2021-11-07 22:36:12 +0800

    transition

--- vue3-geekbang/geek-admin/src/components/Todolist.vue
+++ vue3-geekbang/geek-admin/src/components/Todolist.vue
@@ -2,10 +2,12 @@
     <div>
         <input type="text" v-model="title" @keydown.enter="addTodo" />
         <ul v-if="todos.length">
-            <li v-for="todo in todos">
-                <input type="checkbox" v-model="todo.done" />
-                <span :class="{ done: todo.done }">{{ todo.title }}</span>
-            </li>
+            <transition-group name="flip-list" tag="ul">
+                <li v-for="todo in todos" :key="todo.title">
+                    <input type="checkbox" v-model="todo.done" />
+                    <span :class="{ done: todo.done }">{{ todo.title }}</span>
+                </li>
+            </transition-group>
         </ul>
         <div v-else>暂无数据</div>
         <div>
@@ -13,24 +15,39 @@
             <input type="checkbox" v-model="allDone" />
             <span>{{ active }}/{{ all }}</span>
         </div>
+        <transition name="model">
+            <div class="info-wrapper" v-if="showModel">
+                <div class="info">哥，你啥也没输入</div>
+            </div>
+        </transition>
     </div>
 </template>
 <script setup>
 import { ref, computed } from "vue";
 
-let { title, todos, addTodo, clear, active, all, allDone } = useTodo();
+let { title, todos, showModel, addTodo, clear, active, all, allDone } = useTodo();
 
 
 
 function useTodo() {
     let title = ref("");
     let todos = ref([{ title: "学习Vue", done: false }]);
+    let showModel = ref(false);
     function addTodo() {
-        todos.value.push({
-            title: title.value,
-            done: false
-        });
-        title.value = "";
+        if (title.value === "") {
+            showModel.value = true;
+            setTimeout(() => {
+                showModel.value = false;
+            }, 1000);
+            return;
+        }
+        else {
+            todos.value.push({
+                title: title.value,
+                done: false
+            });
+            title.value = "";
+        }
     }
     function clear() {
         todos.value = todos.value.filter((v) => !v.done);
@@ -49,7 +66,7 @@ function useTodo() {
             })
         }
     });
-    return { title, todos, addTodo, clear, active, all, allDone }
+    return { title, todos, showModel, addTodo, clear, active, all, allDone }
 }
 
 </script>
@@ -59,5 +76,41 @@ function useTodo() {
 .done {
     color: gray;
     text-decoration: line-through;
+}
+.info-wrapper {
+    position: fixed;
+    top: 20px;
+    width: 200px;
+}
+.info {
+    padding: 20px;
+    color: white;
+    background: #d88986;
+}
+.model-enter-from {
+    opacity: 0;
+    transform: translateY(-60px);
+}
+.model-enter-active {
+    transition: all 0.3s ease;
+}
+.model-leave-to {
+    opacity: 0;
+    transform: translateY(-60px);
+}
+.model-leave-active {
+    transition: all 0.3s ease;
+}
+.flip-list-move {
+    transition: transform 0.8s ease;
+}
+.flip-list-enter-active,
+.flip-list-leave-active {
+    transition: all 1s ease;
+}
+.flip-list-enter-from,
+.flip-list-leave-to {
+    opacity: 0;
+    transform: translateX(30px);
 }
 </style>
--- vue3-geekbang/geek-admin/src/pages/home.vue
+++ vue3-geekbang/geek-admin/src/pages/home.vue
@@ -5,13 +5,12 @@
     <Rate v-model="score" theme="red">
         <div>这是 slot</div>
     </Rate>
+    <Animation />
 </template>
 <script setup>
 import TodoList from '../components/Todolist.vue';
 import Rate from '../components/Rate.vue';
+import Animation from '../components/Animation.vue';
 import { ref } from "vue";
 let score = ref(3);
-function update(num) {
-    score.value = num;
-}
 </script>
