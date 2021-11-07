780842d0892b6186f13d4d29d3b9992fc1b1fc10
TonyInBeijing <yuehaowei1126@gmail.com>
2021-11-07 20:36:37 +0800

    defineProps和defineEmits

--- vue3-geekbang/geek-admin/src/pages/home.vue
+++ vue3-geekbang/geek-admin/src/pages/home.vue
@@ -1,7 +1,17 @@
 <template>
     <h1>这是首页</h1>
     <TodoList />
+    <div>你的评分是：{{ score }}</div>
+    <Rate v-model="score" theme="red">
+        <div>这是 slot</div>
+    </Rate>
 </template>
 <script setup>
 import TodoList from '../components/Todolist.vue';
+import Rate from '../components/Rate.vue';
+import { ref } from "vue";
+let score = ref(3);
+function update(num) {
+    score.value = num;
+}
 </script>
