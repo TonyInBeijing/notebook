fb8caaaee7e8b761450b5646d4d0d376a90338a0
TonyInBeijing <yuehaowei1126@gmail.com>
2022-03-05 22:17:42 +0800

    typescript

---
+++ 公开课/20220224-typescript/src/testUser.ts
@@ -0,0 +1,11 @@
+/**
+ * @description 自动寻找 .d.ts 文件中 TestUser 接口
+ */
+
+let testUser: TestUser = {
+    name: "",
+    age: 20,
+    sex: ""
+}
+
+export default {};
---
+++ 公开课/20220224-typescript/types/index.d.ts
@@ -0,0 +1,9 @@
+/**
+ * @description 类型声明文件
+ */
+
+interface TestUser{
+    name: string,
+    age: number,
+    sex: string
+}
