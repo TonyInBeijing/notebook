968a38d1dae96528f0ccda046bea99cf08aa00c5
TonyInBeijing <yuehaowei1126@gmail.com>
2022-03-15 18:46:01 +0800

    feat(demo/nestjs/cats): export service from module

--- demo/nestjs/src/cats/cats.module.ts
+++ demo/nestjs/src/cats/cats.module.ts
@@ -2,7 +2,7 @@
  * @Author: TonyInBeijing
  * @Date: 2022-03-14 20:00:10
  * @LastEditors: TonyInBeijing
- * @LastEditTime: 2022-03-14 20:08:13
+ * @LastEditTime: 2022-03-14 21:12:16
  * @FilePath: /notebook/demo/nestjs/src/cats/cats.module.ts
  * @Description: 
  * 
@@ -14,6 +14,7 @@ import CatsService from "src/cats/cats.service";
 
 @Module({
     controllers: [CatsController],
-    providers: [CatsService]
+    providers: [CatsService],
+    exports: [CatsService]
 })
 export default class CatsModule { };
