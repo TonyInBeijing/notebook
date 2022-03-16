d4888b4d999a95cfe374d3f8402387c8b7989b80
TonyInBeijing <yuehaowei1126@gmail.com>
2022-03-16 16:48:03 +0800

    feat(demo/nestjs): use filter

--- demo/nestjs/src/cats/cats.controller.ts
+++ demo/nestjs/src/cats/cats.controller.ts
@@ -2,13 +2,13 @@
  * @Author: TonyInBeijing
  * @Date: 2022-03-14 14:28:57
  * @LastEditors: TonyInBeijing
- * @LastEditTime: 2022-03-14 20:08:11
- * @FilePath: /notebook/demo/nestjs/src/cats/cats.controller.ts
+ * @LastEditTime: 2022-03-16 16:22:59
+ * @FilePath: \notebook\demo\nestjs\src\cats\cats.controller.ts
  * @Description: 
  * 
  */
 
-import { Body, Controller, Delete, Get, Param, Post, Put, Query } from "@nestjs/common";
+import { Body, Controller, Delete, Get, HttpException, HttpStatus, Param, Post, Put, Query, UseFilters } from "@nestjs/common";
 import CreateCatDto from "src/cats/dto/create-cat.dto";
 import UpdateCatDto from "./dto/update-cat.dto";
 import ListAllEntities from "./dto/list-all-entities.dto";
@@ -16,14 +16,19 @@ import ListAllEntities from "./dto/list-all-entities.dto";
 import CatsService from "src/cats/cats.service";
 import Cat from "src/cats/interfaces/cat.interface";
 
+import HttpExceptionFilter from "src/common/filters/http-exception.filter";
+
 @Controller("cats")
+// @UseFilters(HttpExceptionFilter) -- 控制器级别过滤器
 export default class CatsController {
     constructor(private readonly catsService: CatsService) { }
     @Get()
     async findAll(@Query() query: ListAllEntities): Promise<Cat[]> {
+        throw new HttpException("Forbidden",HttpStatus.FORBIDDEN);
         return this.catsService.findAll();
     }
     @Post()
+    @UseFilters(HttpExceptionFilter) // 路由层面过滤器
     async create(@Body() createCatDto: CreateCatDto): Promise<void> {
         this.catsService.create(createCatDto);
     }
