65ddb600444c1185069d7076e227c74e7a84ac15
TonyInBeijing <yuehaowei1126@gmail.com>
2022-03-19 11:54:44 +0800

    feat(vue3-geekbang/geek-admin): use g2 chart with vue3

---
+++ vue3-geekbang/geek-admin/src/pages/g2.vue
@@ -0,0 +1,52 @@
+<!--
+ * @Author: TonyInBeijing
+ * @Date: 2022-03-19 11:26:55
+ * @LastEditors: TonyInBeijing
+ * @LastEditTime: 2022-03-19 11:39:29
+ * @FilePath: /notebook/vue3-geekbang/geek-admin/src/pages/g2.vue
+ * @Description: 
+ * 
+-->
+
+<template>
+    <div>
+        <div id="chart1"></div>
+    </div>
+</template>
+<script lang="ts">
+import { defineComponent, onMounted, watch } from "vue";
+import { Chart } from "@antv/g2";
+export default defineComponent({
+    setup(props, context) {
+        const data = [
+            { year: '1951 年', sales: 38 },
+            { year: '1952 年', sales: 52 },
+            { year: '1956 年', sales: 61 },
+            { year: '1957 年', sales: 145 },
+            { year: '1958 年', sales: 48 },
+            { year: '1959 年', sales: 38 },
+            { year: '1960 年', sales: 38 },
+            { year: '1962 年', sales: 38 },
+        ];
+        let chart;
+        onMounted(() => {
+            chart = new Chart({
+                container: "chart1",
+                autoFit: true,
+                height: 300
+            });
+            chart.data(data);
+            chart.scale('sales', {
+                nice: true,
+            });
+            chart.tooltip({
+                showMarkers: false
+            });
+            chart.interaction('active-region');
+            chart.interval().position('year*sales');
+
+            chart.render();
+        });
+    }
+});
+</script>
--- vue3-geekbang/geek-admin/src/router/index.js
+++ vue3-geekbang/geek-admin/src/router/index.js
@@ -1,12 +1,21 @@
+/*
+ * @Author: TonyInBeijing
+ * @Date: 2021-10-27 22:13:05
+ * @LastEditors: TonyInBeijing
+ * @LastEditTime: 2022-03-19 11:41:07
+ * @FilePath: /notebook/vue3-geekbang/geek-admin/src/router/index.js
+ * @Description: 
+ * 
+ */
 import {
     createRouter,
     createWebHashHistory
 } from "./grouter/index";
 import Home from "../pages/home.vue";
 import About from "../pages/about.vue";
+import MyChart from "../pages/g2.vue";
 
-const routes = [
-    {
+const routes = [{
         path: "/",
         name: "Home",
         component: Home
@@ -14,7 +23,7 @@ const routes = [
     {
         path: "/about",
         name: "About",
-        component: About
+        component: MyChart
     },
 ];
 
