c75f55f56113a6d666f6904fb133ef95bf71345e
TonyInBeijing <yuehaowei1126@gmail.com>
2022-10-04 15:15:56 +0800

    删除一些笔记

--- vue3-geekbang/geek-admin/src/pages/fullscreen.vue
+++
@@ -1,8 +0,0 @@
-
-<template>
-    <h1 @click="toggle">click</h1>
-</template>
-<script setup>
-import { useFullscreen } from '@vueuse/core'
-const { isFullscreen, enter, exit, toggle } = useFullscreen();
-</script>
