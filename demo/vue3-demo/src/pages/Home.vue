ee71588b23be889cd85a5787a961d3f859b11e21
TonyInBeijing <yuehaowei1126@gmail.com>
2022-10-06 12:24:10 +0800

    feat: defineProps & defineEmits

--- demo/vue3-demo/src/pages/Home.vue
+++ demo/vue3-demo/src/pages/Home.vue
@@ -2,7 +2,7 @@
  * @Author: TonyInBeijing
  * @Date: 2022-10-04 16:09:07
  * @LastEditors: TonyInBeijing
- * @LastEditTime: 2022-10-04 17:47:37
+ * @LastEditTime: 2022-10-06 12:08:46
  * @FilePath: \notebook\demo\vue3-demo\src\pages\Home.vue
  * @Description: 
  * 
@@ -10,8 +10,13 @@
 <template>
     <h1>这是首页</h1>
     <Todolist></Todolist>
+    <Rate :value="3" theme="black" @onRate="onRate"></Rate>
+    <Rate :value="1" theme="red"></Rate>
 </template>
 
 <script setup>
 import Todolist from '../components/Todolist.vue';
+import Rate from "../components/Rate.vue";
+
+const onRate = num => { console.log(num) };
 </script>
