c97270ef41ada67cc686a6371017972e692a3f64
TonyInBeijing <yuehaowei1126@gmail.com>
2022-10-08 22:46:48 +0800

    feat: vuex state,mutations,getters,actions

--- demo/vue3-demo/src/components/Count.vue
+++ demo/vue3-demo/src/components/Count.vue
@@ -2,23 +2,24 @@
  * @Author: TonyInBeijing
  * @Date: 2022-10-06 12:39:40
  * @LastEditors: TonyInBeijing
- * @LastEditTime: 2022-10-06 13:03:42
- * @FilePath: \notebook\demo\vue3-demo\src\components\Count.vue
+ * @LastEditTime: 2022-10-08 22:17:18
+ * @FilePath: /notebook/demo/vue3-demo/src/components/Count.vue
  * @Description: 
  * 
 -->
 <template>
-    <button @click="addCount">
+    <button @click="asyncCount">
         {{count}}
     </button>
 </template>
 
 <script setup>
 import { computed } from "vue";
-import { useStore } from "../store/miniVuex";
-// import { useStore } from "vuex";
+// import { useStore } from "../store/miniVuex";
+import { useStore } from "vuex";
 const store = useStore();
 let count = computed(() => store.state.count);
 
 const addCount = () => { store.commit('add'); };
+const asyncCount = () => {store.dispatch('asyncAdd')}
 </script>
