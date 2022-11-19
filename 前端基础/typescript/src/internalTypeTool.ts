bd4e62c23cb135b44d7021e43b3095f29a17df1c
TonyInBeijing <yuehaowei1126@gmail.com>
2022-11-19 23:49:16 +0800

    feat: typescript 类型工具

---
+++ 前端基础/typescript/src/internalTypeTool.ts
@@ -0,0 +1,13 @@
+/*
+ * @Author: TonyInBeijing
+ * @Date: 2022-11-19 23:47:38
+ * @LastEditors: TonyInBeijing
+ * @LastEditTime: 2022-11-19 23:48:31
+ * @FilePath: \notebook\前端基础\typescript\src\internalTypeTool.ts
+ * @Description: typescript 类型工具
+ * 
+ */
+
+type A = string; // 声明一个名为 A 的 type
+
+export default {};
