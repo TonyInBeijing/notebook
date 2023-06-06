9cde1522d2f013ed3f03508e82c9e933d1150471
TonyInBeijing <yuehaowei1126@gmail.com>
2023-06-06 17:10:14 +0800

    feat(nest-study): 更新第一篇文档（未完成）

--- nestjs-study/src/main.ts
+++ nestjs-study/src/main.ts
@@ -1,8 +1,20 @@
+/*
+ * @Author: TonyInBeijing
+ * @Date: 2023-06-05 16:33:01
+ * @LastEditors: TonyInBeijing
+ * @LastEditTime: 2023-06-06 10:19:48
+ * @FilePath: /notebook/nestjs-study/src/main.ts
+ * @Description: 
+ * 
+ */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useStaticAssets(join(__dirname, "..", "public"), { prefix: "/static" }) // 配置静态资源访问
   await app.listen(3000);
 }
 bootstrap();
