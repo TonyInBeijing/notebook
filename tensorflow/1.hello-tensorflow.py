4614c97d37cf4b25bf549ecde15a9c5bdc7840b1
TonyInBeijing <yuehaowei1126@gmail.com>
2023-12-25 22:23:38 +0800

    feat(tensorflow): 配置环境

--- tensorflow/1.hello-tensorflow.py
+++ tensorflow/1.hello-tensorflow.py
@@ -2,11 +2,15 @@
 Author: TonyInBeijing
 Date: 2023-12-18 23:06:13
 LastEditors: TonyInBeijing
-LastEditTime: 2023-12-19 23:03:28
+LastEditTime: 2023-12-25 21:32:03
 FilePath: /notebook/tensorflow/1.hello-tensorflow.py
 Description: 
 
 '''
 import tensorflow as tf
 
-print(tf.test.__name__)
+hello = tf.constant("Hello TensorFlow")
+
+sess = tf.Session()
+
+print(sess.run(hello))
